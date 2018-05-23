56f0545eb52fe594fd8f9e210811c8a8dd48a5c8
alexnsouza6 <alexnsouza6@hotmail.com>
2018-05-23 15:57:04 -0300

    Comentando mudanças no controller de sessions

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,15 +4,15 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  def new
-    super
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource/sign_in
-  def create
-    super
-    render 'index'
-  end
+  # def create
+  #   super
+  #   render 'index'
+  # end
 
   # DELETE /resource/sign_out
   # def destroy
