b0be85f36321b5fef41550952de30ec874ca1bfc
alexnsouza6 <alexnsouza6@hotmail.com>
2018-05-28 01:09:17 -0300

    Colocando validações

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :username, :course, :registration, :presence => true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
