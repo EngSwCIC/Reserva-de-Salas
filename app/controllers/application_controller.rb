2f918b101978b6c35172546aa1b48c1d8e32a402
alexnsouza6 <alexnsouza6@hotmail.com>
2018-05-28 01:08:33 -0300

    Adicionando método redirect when sign in do devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+ 	protect_from_forgery with: :exception
+
+  	protected
+    	def after_sign_in_path_for(resource_or_scope)
+      		root_path
+    	end
 end
