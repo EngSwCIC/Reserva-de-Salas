a566773796c158b3584b3b197023af28359402e7
Rodrigo <rodrigo.demetrio@orbistec.com.br>
2018-06-06 18:44:32 -0300

    troquei a rota pro backoffice, dps de add um novo adm

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -13,7 +13,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to backoffice_path
     else
       render 'new'
     end
