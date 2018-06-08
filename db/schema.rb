1dc5662e0c5b2dbe4586f22667be7a3a850edeb9
Tiago Cabral <150150296@aluno.unb.br>
2018-06-08 17:56:21 -0300

    Migrações relativas ao aluguel de salas. Adicionados os relacionamentos com as tabelas de usuarios e salas.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180604205706) do
+ActiveRecord::Schema.define(version: 20180606194001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "appointment_date"
+    t.time "start_time"
+    t.integer "status"
+    t.bigint "user_id"
+    t.bigint "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_appointments_on_room_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
