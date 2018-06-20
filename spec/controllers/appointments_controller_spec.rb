27257d66df5180aac4af8280cfaec1f0c355c6a0
alexnsouza6 <alexnsouza6@hotmail.com>
2018-06-20 18:37:50 -0300

    Implementando teste para destroy

--- spec/controllers/appointments_controller_spec.rb
+++ spec/controllers/appointments_controller_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe AppointmentsController, type: :controller do
     describe 'GET #my_appointments' do
         context 'params are valid' do
-            
             before do 
                 @user = FactoryBot.create(:user)
                 sign_in @user
@@ -19,6 +18,24 @@ RSpec.describe AppointmentsController, type: :controller do
 
             it 'renders my appointments template' do
                 expect(response).to render_template :my_appointments        
+            end
+        end
+    end
+
+    describe 'DELETE #destroy' do
+        context 'when appointment exists' do
+            let(:appointment_params) {FactoryBot.attributes_for(:appointment)}
+            before do
+                @user = FactoryBot.create(:user)
+                sign_in @user
+                @room = FactoryBot.create(:room)
+                @appointment = FactoryBot.create(:appointment, :user_id => @user.id, :room_id => @room.id)
+                get :my_appointments
+                delete :destroy
+            end
+
+            it 'deletes a appointment' do
+                expect(Appointment.find_by(appointment_date: appointment_params[:appointment_date])).not_to be_truthy
             end
         end
     end
