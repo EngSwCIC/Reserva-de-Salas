7b2a7233cea495749753bc4b94a9ad2ec95b8635
alexnsouza6 <alexnsouza6@hotmail.com>
2018-06-19 22:52:10 -0300

    Criando rota pro metodo de visualizar alugueis

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
 
   resources :rooms
   resources :appointments
+  get 'my-appointments' => 'appointments#my_appointments'
   get 'backoffice', to: 'backoffice/dashboard#index'
   get 'signup_admin', to: 'backoffice/dashboard#signup_admin'
   post '', to: 'backoffice/dashboard#create', as: :"admin_registration"
