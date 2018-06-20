71aa3f6e027d9de651ae0f632a676f3f2f38faa3
alexnsouza6 <alexnsouza6@hotmail.com>
2018-06-20 16:21:53 -0300

    Comentando parte do id que estava quebrando os testes

--- spec/factories/appointment.rb
+++ spec/factories/appointment.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
         room { create(:room) }
         appointment_date '22-06-2018'
         start_time '10:00:00'
-        user_id user.id
-        room_id room.id
+        # user_id user.id
+        # room_id room.id
     end
 end
