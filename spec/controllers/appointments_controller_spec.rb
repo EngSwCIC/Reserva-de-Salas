fbebd620212982b5ed3a660f611857b1647af09b
alexnsouza6 <alexnsouza6@hotmail.com>
2018-06-20 17:30:54 -0300

    Criando bloco before para criar appointments

--- spec/controllers/appointments_controller_spec.rb
+++ spec/controllers/appointments_controller_spec.rb
@@ -3,13 +3,22 @@ require 'rails_helper'
 RSpec.describe AppointmentsController, type: :controller do
     describe 'GET #my_appointments' do
         context 'params are valid' do
+            
             before do 
-                @my_appointment = FactoryBot.create(:appointment)
-                @my_appointment.user_id = @user.id
-            end
-            it 'expects a appointment in my appointment historic' do
+                @user = FactoryBot.create(:user)
+                sign_in @user
+                @room = FactoryBot.create(:room)
+                @appointment1 = FactoryBot.create(:appointment, :user_id => @user.id, :room_id => @room.id)
+                @appointment2 = FactoryBot.create(:appointment, :user_id => @user.id, :room_id => @room.id)
                 get :my_appointments
-                expect(page).to have_content(@my_appointment)
+            end
+            
+            it 'expects a few appointments in my appointment historic' do
+                expect(assigns(:my_appointments)).to match_array([@appointment1,@appointment2])
+            end
+
+            it 'renders my appointments template' do
+                expect(response).to render_template :my_appointments        
             end
         end
     end
