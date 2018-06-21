9df4c857acd070332fa4d22492bf22ebf1a95a24
Rodrigo Demetrio <rodrigo.demetrio.palma@gmail.com>
2018-06-20 23:54:24 -0300

    Feito o edit do status de um appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -29,5 +29,24 @@ class AppointmentsController < ApplicationController
     @dates = (Date.today.beginning_of_week..Date.today.beginning_of_week+6).map{ |date| date.strftime("%a (%d/%b)") }
   end
 
+  def edit
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+    if @appointment.update(appointment_params)
+      redirect_to request.referrer
+      flash[:notice] = "O aluguél foi editado com sucesso!"
+    else
+      flash[:danger] = "O aluguél não pôde ser editado! Tente novamente!"
+      render 'edit'
+    end
+  end
+
   private
+
+  def appointment_params
+    params.require(:appointment).permit(:status)
+  end
 end
