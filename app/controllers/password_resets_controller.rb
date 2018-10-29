44fcf01a0553b8f96c95020e468a8845bdbb3668
AndreSouto <andre_luisferreira@hotmail.com>
2018-10-29 15:31:55 -0300

    Implementacao da recuperacao de senha por email. Faltam ajustes nas interfaces e testes.

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,63 @@
+class PasswordResetsController < ApplicationController
+  before_action :get_user,         only: [:edit, :update]
+  before_action :valid_user,       only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]    # Case (1)
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Email enviado com instruções para troca de senha."
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Email não encontrado."
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if params[:user][:password].empty?                  # Case (3)
+      @user.errors.add(:password, "Insira uma senha.")
+      render 'edit'
+    elsif @user.update_attributes(user_params)          # Case (4)
+      flash[:success] = "Senha alterada com sucesso."
+      redirect_to user_session_url
+    else
+      render 'edit'                                     # Case (2)
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    # Before filters
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # Confirms a valid user.
+    def valid_user
+       unless (@user)
+        redirect_to root_url
+      end
+    end
+
+    # Checks expiration of reset token.
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Link expirado."
+        redirect_to new_password_reset_url
+      end
+    end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.raise_delivery_errors = true
+
+ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :domain         => 'mail.google.com',
+    :port           => 587,
+    :user_name      => 'reservadesalas2@gmail.com',
+    :password       => 'reservadesalas2018',
+    :authentication => :plain
+}
