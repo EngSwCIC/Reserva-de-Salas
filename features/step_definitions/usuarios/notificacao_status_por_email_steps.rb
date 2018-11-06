9bc8efc2a485f10d5616e2c7a6e27cf6d2669a28
AndreSouto <andre_luisferreira@hotmail.com>
2018-11-05 22:53:15 -0200

    Cucumber completo

---
+++ features/step_definitions/usuarios/notificacao_status_por_email_steps.rb
@@ -0,0 +1,4 @@
+Então("após o pedido ser analisado pelo administrador do sistema eu devo receber em meu email {string}") do |string|
+    # Recebimento do email confirmando ou nao a tentativa de reserva 
+end
+  
