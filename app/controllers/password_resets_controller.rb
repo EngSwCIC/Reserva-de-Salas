f700ed950a9c63423421cb5780f24e85f7ec7ff6
AndreSouto <andre_luisferreira@hotmail.com>
2018-11-08 00:08:40 -0200

    Merge e resolucao de supostos conflitos

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,7 +1,7 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user,         only: [:edit, :update]
   before_action :valid_user,       only: [:edit, :update]
-  before_action :check_expiration, only: [:edit, :update]    # Case (1)
+  #before_action :check_expiration, only: [:edit, :update]    # Case (1)
 
   def new
   end
