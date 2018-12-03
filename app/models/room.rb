35ec8898689339a75f4b60029feb5ae4e0fe521c
Felipe <feliperpasmadjian@hotmail.com>
2018-12-03 11:42:20 -0200

    feature us1-sprint6 test fix

--- app/models/room.rb
+++ app/models/room.rb
@@ -5,4 +5,13 @@ class Room < ApplicationRecord
 	validates :name, presence: true, length: {minimum: 3, maximum: 50}
 	validates :location, presence: true, length: {minimum: 3, maximum: 50}
 	validates :tipo_sala, presence: true, length: {minimum: 3, maximum: 50}
+
+
+	def self.search_by(date, time)
+		if date and time
+			Room.all - Room.joins(:appointments).where('appointment_date = ? AND start_time = ? AND status = 2', date, time)
+		else
+			Room.all
+		end
+	end
 end
