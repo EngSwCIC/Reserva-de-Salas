fd153868c21c77a1e4660e48fa6414d7258b1cfe
Icaro Rezende <icaronr@gmail.com>
2019-05-08 00:10:14 -0300

    factory format updated

--- spec/factories/room.rb
+++ spec/factories/room.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
     factory :room do
-        name 'Fake-123'
-        location 'Fake Location'
+        name        { 'Fake-123' }
+        location    { 'Fake Location' }
+
     end
 end
