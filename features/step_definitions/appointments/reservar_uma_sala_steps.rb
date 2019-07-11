4dc1f6cd5e7de31550e372014f33be4eea432849
Arthur Ribeiro <arthurrib@gmail.com>
2019-07-11 02:24:15 -0300

    reimplementada issue76 por problemas com merge

--- features/step_definitions/appointments/reservar_uma_sala_steps.rb
+++ features/step_definitions/appointments/reservar_uma_sala_steps.rb
@@ -14,11 +14,10 @@ Quando("selecionar o seguinte horário de {string} da manhã") do |string|
   select string, from: "start_time"
 end
 
-Quando("preencher o campo descrição com {string}") do | string |
+Quando("preencher o campo descrição com {string}") do |string|
   fill_in 'description', with: string
 end
 
 Então("deve aparecer uma mensagem na tela dizendo {string}") do |string|
   expect(page).to have_content(string)
 end
-
