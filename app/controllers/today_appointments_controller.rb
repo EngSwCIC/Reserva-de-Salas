7e1eba34a5d05fd8540ada21c7c5a7ac59bf0c01
brenoafb <breno.afb@gmail.com>
2019-11-13 13:48:27 -0300

    Adicionar nome de usuário e nome da sala nas reservas de hoje.

--- app/controllers/today_appointments_controller.rb
+++ app/controllers/today_appointments_controller.rb
@@ -2,6 +2,15 @@ class TodayAppointmentsController < ApplicationController
   def index
     @day = Date.parse(DateTime.now.strftime("%y/%m/%d"))
     @appointments_today = Appointment.where(:appointment_date => @day.beginning_of_day .. @day.end_of_day)
-
   end
+
+  def get_room(id)
+    Room.find(id)
+  end
+
+  def get_user(id)
+    User.find(id)
+  end
+
+  helper_method :get_room, :get_user
 end
