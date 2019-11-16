180270d920bd2184798c997011f429b0d26aabe9
brenoafb <breno.afb@gmail.com>
2019-11-16 11:59:24 -0300

    Remover helpers do controller.

--- app/controllers/today_appointments_controller.rb
+++ app/controllers/today_appointments_controller.rb
@@ -3,14 +3,4 @@ class TodayAppointmentsController < ApplicationController
     @day = Date.parse(DateTime.now.strftime("%y/%m/%d"))
     @appointments_today = Appointment.where(:appointment_date => @day.beginning_of_day .. @day.end_of_day)
   end
-
-  def get_room(id)
-    Room.find(id)
-  end
-
-  def get_user(id)
-    User.find(id)
-  end
-
-  helper_method :get_room, :get_user
 end
