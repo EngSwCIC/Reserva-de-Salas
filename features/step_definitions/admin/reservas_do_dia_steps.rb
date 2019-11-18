d4061057c71416f5298941964976c263d047bbf9
brenoafb <breno.afb@gmail.com>
2019-11-18 16:58:53 -0300

    Corrigir teste de reserva de sala.

--- features/step_definitions/admin/reservas_do_dia_steps.rb
+++ features/step_definitions/admin/reservas_do_dia_steps.rb
@@ -7,6 +7,14 @@ Então("eu devo ver somente a data de hoje") do
   page.has_content?("#{date}")
 end
 
-Quando("preencher o dia do aluguel com a data de hoje") do
-  appointment_date =  Date.today()
+# Quando("preencher o dia do aluguel com a data de hoje") do
+#   # appointment_date =  Date.today()
+#   appointment_date = Date.parse(DateTime.now.strftime("%y-%m-%d"))
+# end
+
+Quando("preencher o dia do aluguel com a data de hoje") do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  table.rows_hash.each do |field, value|
+    fill_in field, :with => Date.today
+  end
 end
