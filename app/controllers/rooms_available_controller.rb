592841f42b9c75a3e8bcb2c42340d6bc2a57066f
Igor Bispo <igor.rabbit99@gmail.com>
2019-12-05 20:41:46 -0300

    Fixado bug print horarios

--- app/controllers/rooms_available_controller.rb
+++ app/controllers/rooms_available_controller.rb
@@ -1,15 +1,14 @@
 class RoomsAvailableController < ApplicationController
   def show
     @room = Room.find(params[:id])
-    @appointments = Appointment.where('appointment_date >= ? AND room_id = ? AND appointment_date <= ?', Date.today, params[:id], Date.today>>1).all
+    @appointments = Appointment.where('appointment_date >= ? AND room_id = ?', Date.today, params[:id]).all
     @dates = (Date.today..Date.today>>1).map{ |date| date.strftime("%a (%d/%b)") }
     options_start = ["06:00", "08:00", "10:00", "12:00", "14:00", "16:00", "18:00", "20:00", "22:00"]
-    @available = Hash[@dates.collect { |date| [date, options_start]}]
+    @available = Hash[@dates.collect { |date| [date, options_start.dup]}]
     @appointments.each do |app|
     	date_now = app.appointment_date.strftime("%a (%d/%b)")
     	if ( (app.status == 2) and (@available[date_now].include?(app.start_time.strftime("%H:%M"))) )
-    		#@available[date_now].delete(app.start_time.strftime ("%H:%M"))
-    		@available[date_now].delete("06:00")
+    		@available[date_now].delete(app.start_time.strftime ("%H:%M"))
     	end
     end
   end 
