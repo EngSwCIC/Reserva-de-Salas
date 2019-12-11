738deeaac02bdeb32bb1269b39cebc147bf0315b
hevslc <32377032+hevslc@users.noreply.github.com>
2019-12-10 21:25:36 -0300

    correção p/ rdoc - TodayAppointmentsController

--- app/controllers/today_appointments_controller.rb
+++ app/controllers/today_appointments_controller.rb
@@ -2,13 +2,17 @@
 ##
 # == Definição da classe *TodayAppointmentsController*.
 # Responsável por identificar os alugueis do dia.
-# Para isso, ocorre a formatação da data para forma ano/mes/dia, em que
-# * ano representa os ultimos dois digitos do ano (ano % 100)
-# * mes completado com zero para resultar dois digitos
-# * dia completado com zero para resultar dois digitos
-#
-# Em seguida, a data é passada para a função _where_ para selecionar apenas alugueis do dia.
+# Métodos:
+# * _index_
 class TodayAppointmentsController < ApplicationController
+  ##
+  # Identifica os alugueis do dia.
+  # Para isso, ocorre a formatação da data para forma ano/mes/dia, em que
+  # * ano representa os ultimos dois digitos do ano (ano % 100)
+  # * mes completado com zero para resultar dois digitos
+  # * dia completado com zero para resultar dois digitos
+  #
+  # Em seguida, a data é passada para a função _where_ para selecionar apenas alugueis do dia, instanciando _appointments_today_...
   def index
     @day = Date.parse(DateTime.now.strftime("%y/%m/%d"))
     @appointments_today = Appointment.where(:appointment_date => @day.beginning_of_day .. @day.end_of_day)
