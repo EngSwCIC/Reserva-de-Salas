04421679f8e1d18fa27f21e2270fd0c43d734dbe
hevslc <32377032+hevslc@users.noreply.github.com>
2019-12-10 21:38:31 -0300

    update RoomsAvailableController p/ rdoc

--- app/controllers/rooms_available_controller.rb
+++ app/controllers/rooms_available_controller.rb
@@ -1,7 +1,7 @@
 ##
 # == Definição da classe *RoomsAvailableController*.
 # Responsável por identificar salas/horários disponíveis para aluguel.
-# Métodos:
+# === Métodos:
 # * _show_.
 
 
@@ -11,7 +11,7 @@ class RoomsAvailableController < ApplicationController
   # Primeiramente, identifica alugueis atuais (cujas datas não são anteriores ao dia atual).
   # Em seguida cria um Hash, para cada data do mês a partir do dia atual, com os vetor de horários disponíveis:
   # ["06:00", "08:00", "10:00", "12:00", "14:00", "16:00", "18:00", "20:00", "22:00"]
-
+  #
   # Em seguida, para cada aluguel ativo, deleta no Hash o horário do dia correspondente.
   # Dessa forma, o Hash possuirá apenas horários disponíveis em datas válidas.
   def show
