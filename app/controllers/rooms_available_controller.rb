7787bf08fc22cf9aa77229f9b24348111910a1d4
Giordano <giordano@suffert.com>
2019-12-11 22:06:42 -0300

    Saikuro, Simplecov e Rubycritics FEITOS

--- app/controllers/rooms_available_controller.rb
+++ app/controllers/rooms_available_controller.rb
@@ -8,6 +8,7 @@
 class RoomsAvailableController < ApplicationController
   ##
   # Identifica salas/horários disponíveis para aluguel.
+  # Salas disponíveis são aquelas sem consulta ou que a consulta não foi confirmada.
   # Primeiramente, identifica alugueis atuais (cujas datas não são anteriores ao dia atual).
   # Em seguida cria um Hash, para cada data do mês a partir do dia atual, com os vetor de horários disponíveis:
   #
@@ -17,15 +18,11 @@ class RoomsAvailableController < ApplicationController
   # Dessa forma, o Hash possuirá apenas horários disponíveis em datas válidas.
   def show
     @room = Room.find(params[:id])
-    @appointments = Appointment.where('appointment_date >= ? AND room_id = ?', Date.today, params[:id]).all
-    @dates = (Date.today..Date.today>>1).map{ |date| date.strftime("%a (%d/%b)") }
-    options_start = ["06:00", "08:00", "10:00", "12:00", "14:00", "16:00", "18:00", "20:00", "22:00"]
-    @available = Hash[@dates.collect { |date| [date, options_start.dup]}]
+    @appointments = helpers.get_future_appointments(params[:id])
+    @available = helpers.get_hash_available_times
     @appointments.each do |app|
     	date_now = app.appointment_date.strftime("%a (%d/%b)")
-    	if ( (app.status == 2) and (@available[date_now].include?(app.start_time.strftime("%H:%M"))) )
-    		@available[date_now].delete(app.start_time.strftime ("%H:%M"))
-    	end
+    	@available[date_now] = helpers.available_times(@available[date_now], app)
     end
   end 
 end
