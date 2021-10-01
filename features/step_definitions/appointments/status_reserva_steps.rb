0282ae9cc11d715f5c819f26b2ff15079c1524ae
Tong00020 <tongzhou2000@gmail.com>
2021-10-01 14:16:14 -0300

    status_reserva_steps new

--- features/step_definitions/appointments/status_reserva_steps.rb
+++ features/step_definitions/appointments/status_reserva_steps.rb
@@ -32,7 +32,12 @@ Dado("que a sala {string}, {string}, {string} não possua requisições no horá
 end
 
 Dado("que a sala {string}, {string}, {string} possua requisições no horário de {string} da manhã") do |name, students, location, horario|
-   # Write code here that turns the phrase above into concrete actions
+  @appointment = Appointment.new
+  @appointment.appointment_date = Date.today + 1
+  @appointment.start_time = horario
+  @appointment.user_id = User.last.id
+  @appointment.room_id = Room.where(name: name, students: students, location: location).last.id
+  @appointment.save
 end
 
 Dado("a sala {string} possua uma requisição aprovada no horário de {string}") do |sala, horario|
