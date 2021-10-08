4e6ab28454454ea39ed0c272832267d579318c18
Yan Galli <yangalli@hotmail.com>
2021-10-08 13:17:25 -0300

    feat: add BDD for list users in dashboard

--- app/controllers/backoffice/dashboard_controller.rb
+++ app/controllers/backoffice/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Backoffice::DashboardController < BackofficeController
 	helper 'backoffice/dashboard'
 
 	def index
+		@users = User.all
+		@rooms = Room.all
+		@appointments = Appointment.all
 	end
 end
