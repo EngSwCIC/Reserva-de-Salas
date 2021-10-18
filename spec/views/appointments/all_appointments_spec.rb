7a8c64fd86d615e3eef78bd65ba7d5a5b5a419d9
Gustavo Tomás <guga.tomas10@gmail.com>
2021-10-18 14:54:50 -0300

    refatoraçao de codigo para FactoryBot

--- spec/views/appointments/all_appointments_spec.rb
+++ spec/views/appointments/all_appointments_spec.rb
@@ -1,21 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'appointments/all_appointments.html.erb', type: :view do
+  let(:user) { FactoryBot.create(:user) }
+  let(:admin_user) { FactoryBot.create(:user, :is_admin => true) }
+
   describe 'POST #update' do
     context 'when admin is signed in' do
-  
+      before do
+        @room = FactoryBot.create(:room)
+        @appointment = FactoryBot.create(:appointment, :user_id => user.id, :room_id => @room.id)
+      end
+      
       # Checks if appointment status is approved
       it 'Estado muda para aprovado' do
         # Approved appointment 0 <- (1) -> 2
-        appointment = Appointment.new(:status => 2)
-        expect(appointment.status).to eq(2)
+        @appointment.status = 2
+        expect(@appointment.status).to eq(2)
       end
   
       # Rafael
       it 'Estado muda para reprovado' do
-        appointment = Appointment.new(:status => 0)
-        expect(appointment.status).to eq(0)
+        @appointment.status = 0
+        expect(@appointment.status).to eq(0)
       end
+
       # Tong
       it 'Estado sem mudança' do
         pending(':status == inicial/default')
