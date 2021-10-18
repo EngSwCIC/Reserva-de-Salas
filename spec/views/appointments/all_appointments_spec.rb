0203d27dd78f48c9cc499e23285f0c9ed6d5c827
Gustavo Tomás <guga.tomas10@gmail.com>
2021-10-18 14:24:21 -0300

    estado aprovado

--- spec/views/appointments/all_appointments_spec.rb
+++ spec/views/appointments/all_appointments_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'appointments/all_appointments.html.erb', type: :view do
-
   describe 'POST #update' do
     context 'when admin is signed in' do
-      # Gustavo
+  
+      # Checks if appointment status is approved
       it 'Estado muda para aprovado' do
-        pending(':status == 1')
+        # Approved appointment 0 <- (1) -> 2
+        appointment = Appointment.new(:status => 2)
+        expect(appointment.status).to eq(2)
       end
+  
       # Rafael
       it 'Estado muda para reprovado' do
         pending(':status == 0')
