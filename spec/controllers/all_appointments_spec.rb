a57c880ce9ca736ea98b763632c771b5c50e95e6
MarcusABR <marcusabrantes1337@gmail.com>
2021-10-19 14:26:25 -0300

    Added reproved case in  controller's all_appointments spec

--- spec/controllers/all_appointments_spec.rb
+++ spec/controllers/all_appointments_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 RSpec.describe AppointmentsController, type: :controller do
   let(:admin) { FactoryBot.create(:user, :is_admin => true) }
 
+  before (:each) do
+    sign_in admin
+    @room = FactoryBot.create(:room)
+  end
+
   context 'Existe uma reserva' do
     before (:each) do
-      sign_in admin
-      @room = FactoryBot.create(:room)
       @appointment = FactoryBot.create(:appointment, :user_id => admin.id, :room_id => @room.id)
       @appointment_credentials = FactoryBot.attributes_for(:appointment, :room_id => @room.id, :format => @room.id)
     end
@@ -17,10 +20,19 @@ RSpec.describe AppointmentsController, type: :controller do
       post "/appointments", params: @appointment_credentials
       expect(@appointment.status).to eq(2)
     end
+  end
 
-    # Marquin
-    it 'Estado reprovado persiste ao recarregar a página' do
-      pending(':status_before_refused == :status_after_refused')
+  # Marquin
+  context 'Quando o status for reprovado' do
+    before (:each) do
+      @appointment = FactoryBot.create(:appointment, :user_id => admin.id, :room_id => @room.id, :status => 0)
+      @appointment_credentials = FactoryBot.attributes_for(:appointment, :room_id => @room.id, :format => @room.id)
+    end
+
+    it 'O status deve continuar como reprovado ao recarregar a página' do
+        within('form') do
+          expect(page).to have_checked_field('Reprovar')
+        end
     end
   end
 end
