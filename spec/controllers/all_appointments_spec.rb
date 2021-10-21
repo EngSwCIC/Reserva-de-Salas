a948840d76555f73f1e3059b9d03e6d085a61bf5
LeoRiether <leonardoalves8217@gmail.com>
2021-10-21 15:07:32 -0300

    rspec: deixei meu teste igual ao do Marcus

--- spec/controllers/all_appointments_spec.rb
+++ spec/controllers/all_appointments_spec.rb
@@ -15,24 +15,20 @@ RSpec.describe AppointmentsController, type: :controller do
     end
 
     # Leo
-    it 'Estado aprovado persiste ao recarregar a página' do
+    it 'O status deve continuar como aprovado ao recarregar a página' do
       @appointment.status = 2 # aprovado
-      post "/appointments", params: @appointment_credentials
-      expect(@appointment.status).to eq(2)
-    end
-  end
-
-  # Marquin
-  context 'Quando o status for reprovado' do
-    before (:each) do
-      @appointment = FactoryBot.create(:appointment, :user_id => admin.id, :room_id => @room.id, :status => 0)
-      @appointment_credentials = FactoryBot.attributes_for(:appointment, :room_id => @room.id, :format => @room.id)
+      within('form') do
+        expect(page).to have_checked_field('Aprovar')
+      end
     end
 
+    # Marquin
     it 'O status deve continuar como reprovado ao recarregar a página' do
-        within('form') do
-          expect(page).to have_checked_field('Reprovar')
-        end
+      @appointment.status = 0 # reprovado
+      within('form') do
+        expect(page).to have_checked_field('Reprovar')
+      end
     end
   end
+
 end
