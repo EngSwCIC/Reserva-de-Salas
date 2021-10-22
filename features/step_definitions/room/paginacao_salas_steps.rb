5e1cfb3b1968e227ba21a6d865ded0808c2923d2
Italo M <italomarcos0010@gmail.com>
2021-10-22 17:20:28 -0300

    testes: 'esteja logado' e 'quero acessar a lista das salas'

--- features/step_definitions/room/paginacao_salas_steps.rb
+++ features/step_definitions/room/paginacao_salas_steps.rb
@@ -7,6 +7,15 @@ Dado("que eu esteja logado como administrador com o email {string} e a senha {st
     click_button "Log in"
 end
 
+E("que eu quero esteja logado") do
+    visit dashboard_path
+    expect(page).to have_text("Signed in as... admin@admin.admin")
+end
+
+E("que eu quero acessar a lista das salas existentes") do
+    expect(current_path).to eq("/backoffice")
+end
+
 Dado("que o banco possui até {int} salas") do |qtd|
     @rooms = Room.all
     expect(@rooms.length).to_be <= qtd
