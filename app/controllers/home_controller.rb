1c7e5f11eb0e984418b32683c82d26684bce7ea7
Felipe Lima <felipesoueu@live.com>
2021-10-21 23:14:39 -0300

    Green Step - Rspec (Reservas Futuras Aprovadas)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
-    
+    @approved = approved_appointments
   end
 
   def approved_appointments
-    
+    Appointment.where(status: 2)
   end
 
 end
