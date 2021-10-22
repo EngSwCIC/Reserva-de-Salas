1ed3763291bbaf3e622641d0e7162be2b1523d7c
Maria Eduarda Holanda <duda.mholanda@gmail.com>
2021-10-22 20:03:23 -0300

    att teste cucumber

--- features/room/paginacao_salas.feature
+++ features/room/paginacao_salas.feature
@@ -7,32 +7,27 @@ Funcionalidade: paginação do index de salas existentes
    Eu quero que o index de salas existentes tenha paginação, sendo exibidas N salas por página.
 
 Contexto:
-   Dado que eu esteja logado como administrador com o email "admin@admin.admin" e a senha "adminseed"
-   E que eu esteja logado
-   E que eu quero acessar a lista das salas existentes
-
-Cenário de Fundo:
-   Dado que o banco possui até 5 salas
+   Dado que eu esteja logado como administrador
+   E que eu esteja na pagina principal
 
 Cenário: A lista é exibida corretamente com 1 página (Happy Path)
+   Dado que o banco possui até 5 salas
    Quando eu clico em "Ver lista de salas existentes"
-   Então eu devo estar na página "/rooms"
-    
-Cenário de Fundo:
-   Dado que o banco possui até 10 salas
+   Então eu devo estar na pagina "/rooms"
 
 Cenário: A lista é exibida corretamente com paginação (Happy Path)
+   Dado que o banco possui até 10 salas
    Quando eu clico em "Ver Listas de Salas Existentes"
-   Então eu devo estar na página "/rooms" de Lista de salas 
-   Dado que a página 2 existe
-   Quando eu clico em página 2
-   Então eu devo estar na página 2 de Lista de salas
+   Então eu devo estar na pagina "/rooms" 
+   Dado que a página "2" existe
+   Quando eu clico em pagina "2"
+   Então eu devo estar na pagina "2"
 
 Cenário: A lista é exibida com paginação, mas são exibidas páginas vazias (Sad Path)
+   Dado que o banco possui até 10 salas
    Quando eu clico em "Ver Listas de Salas Existentes"
-   Então eu devo estar na página 1 de Lista de salas 
-   Quando eu clico em "Próxima página"
-   Então eu devo estar na próxima página de Lista de salas
-   Quando eu clico em "Próxima página"
-   E todas as salas já foram exibidas
-   Então eu devo estar numa página vazia
+   Então eu devo estar na pagina "1"
+   Quando eu clico em "next"
+   Então eu devo estar na página "2"
+   Quando eu tento acessar a pagina "3"
+   Então devo receber um erro
--- features/step_definitions/room/paginacao_salas_steps.rb
+++ features/step_definitions/room/paginacao_salas_steps.rb
@@ -1,21 +1,19 @@
 #paginacao salas steps
 
-Dado("que eu esteja logado como administrador com o email {string} e a senha {string}") do |string, string2|
+Dado("que eu esteja logado como administrador") do
+    @admin = User.where(username: 'rootadmin')[0]
     visit new_user_session_path
-    fill_in :user_email, with: string
-    fill_in :user_password, with: string2
+    fill_in :user_email, with: @admin.email
+    fill_in :user_password, with: @admin.password
     click_button "Log in"
 end
 
-E("que eu esteja logado") do
+E("que eu esteja na pagina principal") do
     visit dashboard_path
     expect(page).to have_text("Signed in as... admin@admin.admin")
 end
 
-E("que eu quero acessar a lista das salas existentes") do
-    expect(current_path).to eq("/backoffice")
-end
-
+# Happy Path 1
 Dado("que o banco possui até {int} salas") do |qtd|
     @rooms = Room.all
     expect(@rooms.length).to_be <= qtd
@@ -29,25 +27,33 @@ Então("eu devo estar na pagina {string}") do |string|
     expect(current_path).to eq("#{string}")
 end
 
+# Happy Path 2
 Dado("que o banco possui até {int} salas") do |qtd|
     @rooms = Room.all
     expect(@rooms.length).to_be <= qtd
+end
+
+Quando("eu clico em {string}") do |string|
+    click_link (string)
+end
+
+Então("eu devo estar na pagina {string}") do |string|
+    expect(current_path).to eq("#{string}")
 end
 
 Dado("que a página {string} existe") do |page|
     expect(get "index", :params => {:page => page}).not_to raise_error(Pagy::Overflow)
 end
 
-Quando("eu clico em página {int}") do |page|
+Quando("eu clico em pagina {string}") do |page|
     click_link (page)
 end
 
-Então("eu devo estar na página {int}") do |page|
-    expect(current_path).to eq("/users/show?page=#{string}")
+Então("eu devo estar na pagina {string}") do |page|
+    expect(current_path).to eq("/rooms?page=#{page}")
 end
 
-# Sad Path {int} salas 
-
+# Sad Path
 Dado("que o banco possui até {int} salas") do |qtd|
     @rooms = Room.all
     expect(@rooms.length).to_be <= qtd
@@ -57,30 +63,22 @@ Quando("eu clico em {string}") do |string|
     click_link (string)
 end
 
-Então("eu devo estar na página {int}") do |page|
-    expect(current_path).to eq("/users/show?page=#{string}")
+Então("eu devo estar na pagina {string}") do |page|
+    expect(current_path).to eq("/rooms?page=#{page}")
 end
 
 Quando("eu clico em {string}") do |string|
     click_link (string)
 end
 
-Então("eu devo estar na próxima página de Lista de salas") do
-    expect(current_path).to eq("/users/show?page=2")
+Então("eu devo estar na página {string}") do |page|
+    expect(current_path).to eq("/rooms?page=#{page}")
 end
 
-Quando("eu clico em {string}") do |string|
-    click_link (string)
+Quando("eu tento acessar a pagina {string}") do |page|
+    visit("/rooms?page=#{page}")
 end
 
-E("todas as salas já foram exibidas") do
-    click_link (string)
+Então("devo receber um erro") do
+    expect {get "index", :params => {:page => "3"}}.to raise_error(Pagy::OverflowError)
 end
-
-Então("eu devo estar numa página vazia") do
-    @rooms = Room.all
-    expect(@rooms.length).to_be == 0
-end
-
-
-
