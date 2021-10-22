86df979f0de3d6ab4d3c287cad3228e68bfdfed4
Smylle3 <joao21731@gmail.com>
2021-10-21 21:55:59 -0300

    Fiz o rspec e implementei a feature

--- app/controllers/backoffice/dashboard_controller.rb
+++ app/controllers/backoffice/dashboard_controller.rb
@@ -2,5 +2,9 @@ class Backoffice::DashboardController < BackofficeController
 	helper 'backoffice/dashboard'
 
 	def index
+		@users = User.all
+		@rooms = Room.all
+		@appointments = Appointment.all
+		@appointments_aprove = Appointment.where(status: 2)
 	end
 end
