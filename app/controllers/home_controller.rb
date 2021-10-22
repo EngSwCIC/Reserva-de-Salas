f0a40d3114bf252e723bd7d8f9b327d513cbaa15
mitikoaline <mitikoarine@gmail.com>
2021-10-22 14:09:08 -0300

    Adicionando rspec - reservas antigas e expiradas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,14 @@
 class HomeController < ApplicationController
   def index
     @approved = approved_appointments
+    @expired = expired_appointments
   end
 
   def approved_appointments
     Appointment.where(status: 2).order('appointment_date ASC')
   end
 
+  def expired_appointments
+    Appointment.where("appointment_date < ?", Date.today)
+  end
 end
