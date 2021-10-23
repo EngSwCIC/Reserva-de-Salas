c7e3e5bba2427f745d9c1976160b90dc547320cc
daniloinacio <daniloinaciossilva@gmail.com>
2021-10-22 21:44:09 -0300

    Alterações na view e na controller para implementar a feature reservas pendentes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,17 @@ class HomeController < ApplicationController
   def index
     @approved = approved_appointments
     @expired = expired_appointments
+    @pendent = pendent_appointments
   end
 
   def approved_appointments
     Appointment.where(status: 2).order('appointment_date ASC')
   end
 
+  def pendent_appointments
+    Appointment.whele(status: 1).order("appointment_data ASC")
+  end
+  
   def expired_appointments
     Appointment.where("appointment_date < ?", Date.today)
   end
