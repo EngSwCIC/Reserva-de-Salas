0429542468f7fcd683dca179adaa3022a7f5d49c
MarcusABR <marcusabrantes1337@gmail.com>
2021-11-01 11:13:18 -0300

    modified spec for all_appointments

--- spec/views/appointments/all_appointments_spec.rb
+++ spec/views/appointments/all_appointments_spec.rb
@@ -9,27 +9,35 @@ RSpec.describe 'appointments/all_appointments.html.erb', type: :view do
       # Approved appointment 0 <- (1) -> 2
       before do
         @room = FactoryBot.create(:room)
+        @appointments = FactoryBot.create_list(:appointment, 2, :user_id => user.id, :room_id => @room.id)
+        
         @appointment = FactoryBot.create(:appointment, :user_id => user.id, :room_id => @room.id)
+      end
+
+      describe "Routes", :type => :routing do
+        it 'A rota para status existe' do
+          expect(:get => change_status_path).
+            to route_to(:controller => 'appointments', :action => 'status') 
+        end
       end
       
       # Marcus
       # Checks if appointment status is approved
       it 'Estado muda para aprovado' do
+        render
         @appointment.status = 2
-        expect(@appointment.status).to eq(2)
+        @appointment.save
+        #:appoitment.status = 2
+        rendered.should have_tag("#status#{@appointment.id}", text: "Aprovado")
       end
   
       # Gustavo
-      describe "Routes", :type => :routing do
-        it 'Estado muda para reprovado' do
-          # Rota para status funciona
-          expect(:get => change_status_path).
-            to route_to(:controller => 'appointments', :action => 'status')  
-          # Mudança para reprovado é salva
-          @appointment.status = 0
-          @appointment.save
-          expect(@appointment.status).to eq(0)
-        end
+      
+      it 'Estado muda para reprovado' do
+        # Mudança para reprovado é salva
+        @appointment.status = 0
+        @appointment.save
+        expect(@appointment.status).to eq(0)
       end
 
       # Tong
