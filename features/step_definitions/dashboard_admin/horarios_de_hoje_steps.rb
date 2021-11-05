b6f138dd1d09536039bc962313ba59ae25d55f70
Yan Galli <yangalli@hotmail.com>
2021-11-05 00:18:08 -0300

    refactor em horarios de hoje

--- features/step_definitions/dashboard_admin/horarios_de_hoje_steps.rb
+++ features/step_definitions/dashboard_admin/horarios_de_hoje_steps.rb
@@ -6,12 +6,12 @@ end
 
 Dado('que eu tenha aprovado uma reserva de um usuário para hoje no horario {int}:{int}') do |hora, minuto|
     room = Room.create(name: 'Sala 1', location: 'CIC',  students: 100)
-    start_time = Time.new(2000, 1, 1, hora, minuto, 0, "UTC")
+    start_time = Time.new(2000, 1, 1, hora, minuto, 0, "+00:00")
     appointment = Appointment.create(room_id: room.id, user_id: User.where(is_admin: true).first.id, appointment_date: DateTime.current.beginning_of_day, start_time: start_time, description: 'Horario de teste', status: 2)
     expect(Appointment.all.length).to eq 1
 end
 
-Então('eu devo ver {int}:{int} na tabela de horários') do |hora, minuto|    
+Então('eu devo ver {int}:{int} na tabela de horários') do |hora, minuto|
     expect(page.has_content?("#{hora}:#{minuto}")).to be true
 end
 
