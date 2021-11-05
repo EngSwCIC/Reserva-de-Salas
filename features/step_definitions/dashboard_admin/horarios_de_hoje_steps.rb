6f2abf0e4aee527dd663e0d9536d301af42bf99f
Sergio Alonso Jr <sergioawoke@gmail.com>
2021-11-04 22:11:14 -0300

    Refactore para reduzir a complexidade.

--- features/step_definitions/dashboard_admin/horarios_de_hoje_steps.rb
+++ features/step_definitions/dashboard_admin/horarios_de_hoje_steps.rb
@@ -1,15 +1,13 @@
+require 'date'
+
 E ('meu usuário admin é válido') do
     expect(User.where(is_admin: true).length).to be > 0
 end
 
 Dado('que eu tenha aprovado uma reserva de um usuário para hoje no horario {int}:{int}') do |hora, minuto|
     room = Room.create(name: 'Sala 1', location: 'CIC',  students: 100)
-    require 'date'
-    today = Date.today
-    today_date = Time.new(today.year, today.month, today.day, 0, 0, 0, "UTC")
     start_time = Time.new(2000, 1, 1, hora, minuto, 0, "UTC")
-    appointment = Appointment.create(room_id: room.id, user_id: User.where(is_admin: true).first.id, appointment_date: today_date, start_time: start_time, description: 'Horario de teste', status: 2)
-    appointment.save!
+    appointment = Appointment.create(room_id: room.id, user_id: User.where(is_admin: true).first.id, appointment_date: DateTime.current.beginning_of_day, start_time: start_time, description: 'Horario de teste', status: 2)
     expect(Appointment.all.length).to eq 1
 end
 
